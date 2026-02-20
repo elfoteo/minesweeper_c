92b03d2268d276f233c134b0caa78a90ead4ae7d
elfoteo <matteociocci08@gmail.com>
2026-02-20 13:29:42 +0000

    Fixed textboxes

--- src/main_menu.c
+++ src/main_menu.c
@@ -3,6 +3,8 @@
 #include "main.h"
 #include <raylib.h>
 #include <raymath.h>
+#include <stdbool.h>
+#include <string.h>
 
 #define BUTTON_COUNT 4
 #define BUTTON_HEIGHT 60
@@ -30,7 +32,10 @@ typedef struct {
     ButtonId id;
 } MenuButton;
 
-MenuButton buttons[] = {{"Play", BTN_PLAY}, {"Settings", BTN_SETTINGS}, {"About", BTN_ABOUT}, {"Exit", BTN_EXIT}};
+static MenuButton buttons[] = {{"Play", BTN_PLAY}, {"Settings", BTN_SETTINGS}, {"About", BTN_ABOUT}, {"Exit", BTN_EXIT}};
+// No clue on how you want to handle the results for each setting, rn its an array, but i'd use a var for every field
+#define MAX_STRING_LEN 6 // INCLUDING NULLBYTE, IMPORTANT!
+static char textboxes_strings[BUTTON_COUNT][MAX_STRING_LEN] = {0};
 
 static int random_weighted_number() {
     int r = GetRandomValue(0, 99);
@@ -235,21 +240,15 @@ void main_menu_draw() {
     }
 }
 
-char *textbox(int cursor) {
-    Rectangle textbox = {SCREEN_WIDTH / 2 - 100, cursor, 200, BUTTON_HEIGHT};
-    char name[5 + 1] = "\0";
+/* Returns if hovered or not */
+bool draw_textbox(char *name, int max_lenght, int x, int y, int w, int h) {
+    Rectangle textbox = {x - (float)w / 2, y - (float)h / 2, w, h};
     bool mouseOnText = false;
-    int letterCount = 0;
+    int letterCount = strlen(name);
+    bool hovered = false;
 
     if (CheckCollisionPointRec(GetMousePosition(), textbox)) {
-        mouseOnText = true;
-    } else {
-        mouseOnText = false;
-    }
-
-    if (mouseOnText) {
-        // Set the window's cursor to the I-Beam
-        SetMouseCursor(MOUSE_CURSOR_IBEAM);
+        hovered = true;
 
         // Get char pressed (unicode character) on the queue
         int key = GetCharPressed();
@@ -257,7 +256,7 @@ char *textbox(int cursor) {
         // Check if more characters have been pressed on the same frame
         while (key > 0) {
             // NOTE: Only allow keys in range [32..125]
-            if ((key >= 32) && (key <= 125) && (letterCount < 5)) {
+            if ((key >= 32) && (key <= 125) && (letterCount < max_lenght)) {
                 name[letterCount] = (char)key;
                 name[letterCount + 1] = '\0'; // Add null terminator at the end of the string
                 letterCount++;
@@ -272,11 +271,11 @@ char *textbox(int cursor) {
                 letterCount = 0;
             name[letterCount] = '\0';
         }
-    } else
-        SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+    }
+
     DrawRectangleRec(textbox, GRAY);
     DrawText(name, (int)textbox.x + 5, (int)textbox.y + 8, 40, WHITE);
-    return name;
+    return hovered;
 }
 
 void settings_menu_draw() {
@@ -292,9 +291,16 @@ void settings_menu_draw() {
 
     // Leave some space between buttons and title
     cursor += BUTTON_SPACING * 6;
+    bool something_hovered = false;
     for (int i = 0; i < BUTTON_COUNT; i++) {
-        textbox(cursor);
+        something_hovered |= draw_textbox(textboxes_strings[i], 5, SCREEN_WIDTH / 2, cursor, 200, BUTTON_HEIGHT);
         // later on we can put that bomb number = textbox or something
         cursor += BUTTON_SPACING + BUTTON_HEIGHT;
+    }
+    if (something_hovered) {
+        // Set the window's cursor to the I-Beam
+        SetMouseCursor(MOUSE_CURSOR_IBEAM);
+    } else {
+        SetMouseCursor(MOUSE_CURSOR_DEFAULT);
     }
 }
