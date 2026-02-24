dfe45903b9d089008ddbc3a459083e7ef8457179
Pako-it-is <pakoslawkonert@gmail.com>
2026-02-24 09:09:45 +0000

    Fixed win/lose messages

--- src/screens/loose_screen.c
+++ src/screens/loose_screen.c
@@ -7,7 +7,7 @@ void screen_loose_draw() {
     ClearBackground(BLACK); // TODO change this for something better
 
     DrawText("YOU LOST", 0, 0, 4 * fontsize, GRAY);
-    DrawText("Smart Boi!!(or girl(or person))", 0, 4 * fontsize, fontsize, GRAY);
+    DrawText("Better luck next time champ.", 0, 4 * fontsize, fontsize, GRAY);
 
     if (button_draw_centered("Back", SCREEN_WIDTH / 2, SCREEN_HEIGHT - 140, 200, 60, BUTTON_DEFAULT_STYLE)) {
         game_state = STATE_MAIN_MENU;
--- src/screens/win_screen.c
+++ src/screens/win_screen.c
@@ -7,7 +7,7 @@ void screen_win_draw() {
     ClearBackground(BLACK); // TODO change this for something better
 
     DrawText("YOU WON", 0, 0, 4 * fontsizewin, YELLOW);
-    DrawText("As a reward, you can close this window.", 0, 4 * fontsizewin, fontsizewin, YELLOW);
+    DrawText("Smart Boi!!(or girl(or person))", 0, 4 * fontsizewin, fontsizewin, YELLOW);
 
     if (button_draw_centered("Back", SCREEN_WIDTH / 2, SCREEN_HEIGHT - 140, 200, 60, BUTTON_DEFAULT_STYLE)) {
         game_state = STATE_MAIN_MENU;
